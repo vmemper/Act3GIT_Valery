f777d721732d977872a79b6981b804cebd3fb3e5
Valery Nayeuska Membreño Pérez <vmemper219@g.educaand.es>
2024-11-21 13:21:31 +0100

    Cambio en el programa se agrega valor3 y si esta entre valor1 y valor2

--- proyectoMetodos/src/proyectoMetodos/MetodosAct3.java
+++ proyectoMetodos/src/proyectoMetodos/MetodosAct3.java
@@ -11,19 +11,33 @@ public class MetodosAct3 {
 		
 		int valor1=0;
 		int valor2=0;
+		int valor3=0;
+		int valorInicial=0;
+		
 		do {
-			System.out.println("Introuduce 2 valores por teclado");
+			System.out.println("Introuduce 3 valores por teclado");
 			System.out.println("Valor 1");
 			valor1 = Integer.parseInt(sc.nextLine());
 			System.out.println("Valor 2");
 			valor2 = Integer.parseInt(sc.nextLine());
+			System.out.println("Valor 3");
+			valor3 = Integer.parseInt(sc.nextLine());
 		
 			while (valor1 + 7 <= valor2) {
                 valor1 += 7;
+                
+                if(valorInicial==0) {
+                valorInicial=valor1;
+                }
                 int valorAnterior=valor1-7;
                 System.out.println(valorAnterior +" + 7 = "+ valor1);
             }	
-			System.out.println("Entre los valores tiene que haber mímino una diferencia de 7");
+			 
+			if(valor3>valorInicial && valor3<valor1) {
+				System.out.println("El valor3 está entre el valor1 y valor2");
+			}
+			
+			System.out.println("Para realizar la operacion entre los valores tiene que haber mímino una diferencia de 7");
 		} while(valor1<=valor2);
 		
 		sc.close();
